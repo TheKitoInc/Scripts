82b8cdb3f8118ecc87513ba4e23768e9122591a9
TheKito <11139894+TheKito@users.noreply.github.com>
2023-03-17 14:39:43 -0300

    Create username.bat

---
+++ Domain/username.bat
@@ -0,0 +1,11 @@
+@echo off
+cls
+echo Parse User Name
+CALL :LoCase USERNAME
+GOTO:EOF
+:LoCase
+:: Subroutine to convert a variable VALUE to all lower case.
+:: The argument for this subroutine is the variable NAME.
+FOR %%i IN ("A=a" "B=b" "C=c" "D=d" "E=e" "F=f" "G=g" "H=h" "I=i" "J=j" "K=k" "L=l" "M=m" "N=n" "O=o" "P=p" "Q=q" "R=r" "S=s" "T=t" "U=u" "V=v" "W=w" "X=x" "Y=y" "Z=z") DO CALL SET "%1=%%%1:%%~i%%"
+GOTO:EOF
+:EOF
