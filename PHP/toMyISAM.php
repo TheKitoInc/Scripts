950177cec70dbfafa9a510607b01c4fceb8d605b
TheKito <11139894+TheKito@users.noreply.github.com>
2023-03-17 14:41:29 -0300

    Create toMyISAM.php

---
+++ PHP/toMyISAM.php
@@ -0,0 +1,27 @@
+<?php
+  $cnn = mysqli_connect('127.0.0.1','root','');
+  
+  $sql = "SELECT TABLE_SCHEMA,TABLE_NAME FROM INFORMATION_SCHEMA.TABLES WHERE ENGINE = 'InnoDB'";
+  $rs = $cnn->query($sql);
+
+  $cnn->query('SET foreign_key_checks = 0;');
+
+  while($row = $rs->fetch_assoc())
+  {
+      $shm = $row['TABLE_SCHEMA'];
+      $tbl = $row['TABLE_NAME'];
+
+      $sql = "ALTER TABLE `$shm`.`$tbl` ENGINE=MyISAM";
+      var_dump($sql);
+    
+      try
+      {
+        $cnn->query($sql);
+      }
+			catch(\Exception $e)
+			{
+				print_r($e);
+			}
+  }
+
+  $cnn->query('SET foreign_key_checks = 1;');
