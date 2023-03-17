0cb4002d8498cfa1785c0fe21a9688d0a1f9aad5
TheKito <11139894+TheKito@users.noreply.github.com>
2023-03-17 14:40:13 -0300

    Create DebianSetHostNameByPTR.sh

---
+++ Installers/DebianSetHostNameByPTR.sh
@@ -0,0 +1,3 @@
+#/bin/bash
+
+NEWHOSTNAME=$(python3 -c "import sys, requests, socket; hostname=socket.gethostbyaddr(requests.get('https://ipv4.jsonip.com/').json()['ip'])[0].split('.');hostname[0]=hostname[0].upper();hostnameSTR='.'.join(hostname);print(hostnameSTR)") && hostnamectl set-hostname "$NEWHOSTNAME"
