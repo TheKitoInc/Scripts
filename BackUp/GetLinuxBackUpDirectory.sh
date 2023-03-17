5eab361abd049831c25334bbe2151cc6587bb590
TheKito <11139894+TheKito@users.noreply.github.com>
2023-03-17 14:37:32 -0300

    Create GetLinuxBackUpDirectory.sh

---
+++ BackUp/GetLinuxBackUpDirectory.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+HOST=$1
+NAME=$(echo "$HOST" | cut -d "." -f1)
+
+ssh-copy-id $HOST
+
+SRC=$1:/
+DST=/Storage/Master/$NAME/
+HST=/Storage/History/$NAME/$(date "+%Y/%m/%d/%H")/
+
+echo SRC: $SRC
+echo DST: $DST
+echo HST: $HST
+
+mkdir -p "$DST" && \
+mkdir -p "$HST" && \
+rsync -arv --exclude={"/var/lib/mlocate","/var/mail","/var/log","/var/lib/ispell","/var/lib/dpkg","/var/lib/apt","/var/cache","/bin","/boot","/dev","/initrd*","/lib*","/mount","/sbin","/srv","/usr","vmlinuz*","/proc","/sys","/tmp","/run","/mnt","/media","/lost+found","/Storage"} --delete-after --delete -b --backup-dir="$HST" "$SRC" "$DST" && \
+find "$HST" -type f -iname "*.bz2" -exec bunzip2 -fv "{}" \; && \
+fdupes -drNI "$HST" && \
+find "$HST" -type f ! -name "*.gz" ! -name "*.bz2" -exec gzip -fv "{}" \; && \
+find "$HST" -type f -exec touch "{}" \;
