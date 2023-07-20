f2b2ffd58073923b7938a1eb8dc0bfa6bdfc8c7f
TheKitoInc <11139894+TheKito@users.noreply.github.com>
2023-07-20 14:58:59 -0300

    Update InstallMaildir.sh

--- Installers/Postfix/InstallMaildir.sh
+++ Installers/Postfix/InstallMaildir.sh
@@ -4,7 +4,12 @@ groupadd -g 5000 vmail
 useradd -u 5000 -g vmail -s /usr/sbin/nologin -d  /var/mail/maildir/ -m vmail
 
 touch /etc/postfix/virtual_mailbox_maps
+touch /etc/postfix/virtual_mailbox_domains
+
+cat /etc/postfix/virtual_mailbox_maps | cut -d' ' -f1 | cut -d'@' -f2 | sort | uniq > /etc/postfix/virtual_mailbox_domains
+
 postmap /etc/postfix/virtual_mailbox_maps
+postmap /etc/postfix/virtual_mailbox_domains
 
 postconf 'virtual_minimum_uid = 1000'
 postconf 'virtual_uid_maps = static:5000'
@@ -12,6 +17,6 @@ postconf 'virtual_gid_maps = static:5000'
 postconf 'virtual_mailbox_limit = 5368709120'
 postconf 'virtual_mailbox_base = /var/mail/maildir/'
 postconf 'virtual_mailbox_maps = hash:/etc/postfix/virtual_mailbox_maps'
-postconf 'virtual_mailbox_domains = $virtual_mailbox_maps'
+postconf 'virtual_mailbox_domains = /etc/postfix/virtual_mailbox_domains'
 
 /etc/init.d/postfix reload
