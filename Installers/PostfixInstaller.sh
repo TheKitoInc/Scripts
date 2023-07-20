f92046a7ab9ab88ce3c648957bf18cfa21d57288
TheKitoInc <11139894+TheKito@users.noreply.github.com>
2023-07-20 10:32:53 -0300

    Update PostfixInstaller.sh
    add mailbox

--- Installers/PostfixInstaller.sh
+++ Installers/PostfixInstaller.sh
@@ -101,4 +101,20 @@ touch /etc/postfix/virtual_alias_domains
 touch /etc/postfix/virtual_alias_maps
 postmap /etc/postfix/virtual_alias_maps
 
+#MailBox
+groupadd -g 5000 vmail
+useradd -u 5000 -g vmail -s /usr/bin/nologin -d  /var/mail/maildir/ -m vmail
+
+touch /etc/postfix/virtual_mailbox_maps
+postmap /etc/postfix/virtual_mailbox_maps
+
+postconf 'virtual_minimum_uid = 1000'
+postconf 'virtual_uid_maps = static:5000'
+postconf 'virtual_gid_maps = static:5000'
+postconf 'virtual_mailbox_limit = 5368709120'
+postconf 'virtual_mailbox_base = /var/mail/maildir/'
+postconf 'virtual_mailbox_maps = hash:/etc/postfix/virtual_mailbox_maps'
+postconf 'virtual_mailbox_domains = $virtual_mailbox_maps'
+#
+
 /etc/init.d/postfix reload
