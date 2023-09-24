b066b39f6c6d5f1c69f223c15ee422f8234a9313
TheKito <11139894+TheKito@users.noreply.github.com>
2023-09-24 15:40:32 -0300

    Update OpenVPNNode.sh

--- Install/OpenVPNNode.sh
+++ Install/OpenVPNNode.sh
@@ -30,30 +30,31 @@ CNServer=$CNClient.$(hostname --fqdn | cut -d. -f2-255)
 apt-get update || exit 1
 apt-get install openvpn -y || exit 2
 apt-get install easy-rsa -y || exit 3
+apt-get install php-cli -y || exit 5
 
-[ -d "$PKI/pki" ] || (cd "$PKI" && $EASYRSA init-pki) || exit 4
-[ -f "$PKI/pki/dh.pem" ] || (cd "$PKI" && $EASYRSA gen-dh) || exit 5
+[ -d "$PKI/pki" ] || (cd "$PKI" && $EASYRSA init-pki) || exit 5
+[ -f "$PKI/pki/dh.pem" ] || (cd "$PKI" && $EASYRSA gen-dh) || exit 6
 
-[ -f "$PKI/pki/reqs/$CNClient.req" ] || (cd "$PKI" && $EASYRSA gen-req $CNClient nopass) || exit 6
-[ -f "$PKI/pki/reqs/$CNServer.req" ] || (cd "$PKI" && $EASYRSA gen-req $CNServer nopass) || exit 7
+[ -f "$PKI/pki/reqs/$CNClient.req" ] || (cd "$PKI" && $EASYRSA gen-req $CNClient nopass) || exit 7
+[ -f "$PKI/pki/reqs/$CNServer.req" ] || (cd "$PKI" && $EASYRSA gen-req $CNServer nopass) || exit 8
 
-[ -L "$PKI/pki/reqs/client.req" ] || (cd "$PKI/pki/reqs/" && ln -s $CNClient.req client.req) || exit 8
-[ -L "$PKI/pki/reqs/server.req" ] || (cd "$PKI/pki/reqs/" && ln -s $CNServer.req server.req) || exit 9
+[ -L "$PKI/pki/reqs/client.req" ] || (cd "$PKI/pki/reqs/" && ln -s $CNClient.req client.req) || exit 9
+[ -L "$PKI/pki/reqs/server.req" ] || (cd "$PKI/pki/reqs/" && ln -s $CNServer.req server.req) || exit 10
 
-[ -L "$PKI/pki/private/client.key" ] || (cd "$PKI/pki/private/" && ln -s $CNClient.key client.key) || exit 10
-[ -L "$PKI/pki/private/server.key" ] || (cd "$PKI/pki/private/" && ln -s $CNServer.key server.key) || exit 11
+[ -L "$PKI/pki/private/client.key" ] || (cd "$PKI/pki/private/" && ln -s $CNClient.key client.key) || exit 11
+[ -L "$PKI/pki/private/server.key" ] || (cd "$PKI/pki/private/" && ln -s $CNServer.key server.key) || exit 12
 
-[ -d "$PKI/pki/issued/" ] || (mkdir "$PKI/pki/issued/") || exit 12
+[ -d "$PKI/pki/issued/" ] || (mkdir "$PKI/pki/issued/") || exit 13
 
-[ -L "$PKI/pki/issued/client.crt" ] || (cd "$PKI/pki/issued/" && ln -s $CNClient.crt client.crt) || exit 13
-[ -L "$PKI/pki/issued/server.crt" ] || (cd "$PKI/pki/issued/" && ln -s $CNServer.crt server.crt) || exit 14
+[ -L "$PKI/pki/issued/client.crt" ] || (cd "$PKI/pki/issued/" && ln -s $CNClient.crt client.crt) || exit 14
+[ -L "$PKI/pki/issued/server.crt" ] || (cd "$PKI/pki/issued/" && ln -s $CNServer.crt server.crt) || exit 15
 
-[ ! -d "/etc/openvpn/server" ] || (rmdir "/etc/openvpn/server") || exit 15
-[ ! -d "/etc/openvpn/client" ] || (rmdir "/etc/openvpn/client") || exit 16
-[ ! -f "/etc/openvpn/update-resolv-conf" ] || (rm "/etc/openvpn/update-resolv-conf") || exit 17
-[ -d "/etc/openvpn/ccd" ] || (mkdir "/etc/openvpn/ccd") || exit 18
+[ ! -d "/etc/openvpn/server" ] || (rmdir "/etc/openvpn/server") || exit 16
+[ ! -d "/etc/openvpn/client" ] || (rmdir "/etc/openvpn/client") || exit 17
+[ ! -f "/etc/openvpn/update-resolv-conf" ] || (rm "/etc/openvpn/update-resolv-conf") || exit 18
+[ -d "/etc/openvpn/ccd" ] || (mkdir "/etc/openvpn/ccd") || exit 19
 
-(cat /etc/sysctl.conf | grep "net.ipv4.ip_forward = 1") || (echo "net.ipv4.ip_forward = 1" >> /etc/sysctl.conf && sysctl -p /etc/sysctl.conf) || exit 19
+(cat /etc/sysctl.conf | grep "net.ipv4.ip_forward = 1") || (echo "net.ipv4.ip_forward = 1" >> /etc/sysctl.conf && sysctl -p /etc/sysctl.conf) || exit 20
 
 CONFIGBASE=$(echo "
 port 1194
