c2b7ae6f7e3e8433876e2322a34cdd2924490315
TheKito <11139894+TheKito@users.noreply.github.com>
2023-09-24 13:13:26 -0300

    Update OpenVPNNode.sh

--- Install/OpenVPNNode.sh
+++ Install/OpenVPNNode.sh
@@ -4,6 +4,8 @@ export DEBIAN_FRONTEND=noninteractive;
 
 EASYRSA=/usr/share/easy-rsa/easyrsa
 PKI=/etc/openvpn
+CNClient=$(hostname)
+CNServer=$(hostname -f)
 
 apt-get update || exit 1
 apt-get install openvpn -y || exit 2
@@ -12,3 +14,17 @@ apt-get install easy-rsa -y || exit 3
 [ -d "$PKI/pki" ] || (cd "$PKI" && $EASYRSA init-pki) || exit 4
 [ -f "$PKI/pki/dh.pem" ] || (cd "$PKI" && $EASYRSA gen-dh) || exit 5 
  
+[ -f "$PKI/pki/reqs/$CNClient.req" ] || (cd "$PKI" && $EASYRSA gen-req $CNClient nopass) || exit 6
+[ -f "$PKI/pki/reqs/$CNServer.req" ] || (cd "$PKI" && $EASYRSA gen-req $CNServer nopass) || exit 7
+
+[ -L "$PKI/pki/reqs/client.req" ] || (cd "$PKI/pki/reqs/" && ln -s $CNClient.req client.req) || exit 8
+[ -L "$PKI/pki/reqs/server.req" ] || (cd "$PKI/pki/reqs/" && ln -s $CNServer.req server.req) || exit 9
+
+[ -L "$PKI/pki/private/client.key" ] || (cd "$PKI/pki/private/" && ln -s $CNClient.key client.key) || exit 10
+[ -L "$PKI/pki/private/server.key" ] || (cd "$PKI/pki/private/" && ln -s $CNServer.key server.key) || exit 11
+
+[ -d "$PKI/pki/issued/" ] || (mkdir "$PKI/pki/issued/") || exit 12
+
+[ -L "$PKI/pki/issued/client.crt" ] || (cd "$PKI/pki/issued/" && ln -s $CNClient.crt client.crt) || exit 13
+[ -L "$PKI/pki/issued/server.crt" ] || (cd "$PKI/pki/issued/" && ln -s $CNServer.crt server.crt) || exit 14
+
