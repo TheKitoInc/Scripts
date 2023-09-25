b3516d8ee5eb90196645e2758bd090d167d4fb5b
TheKito <11139894+TheKito@users.noreply.github.com>
2023-09-25 12:14:47 -0300

    Update index.sh

--- Install/OpenVPNNode/index.sh
+++ Install/OpenVPNNode/index.sh
@@ -156,5 +156,5 @@ curl "$URLFiles/ovpn2json.php" > /opt/kito/ovpn/ovpn2json.php  || exit 28
 curl "$URLFiles/ovpn2json2route.php" > /opt/kito/ovpn/ovpn2json2route.php  || exit 29
 
 
-cat /etc/crontab | grep "/opt/kito/ovpn/ovpn2json.php" || (echo "* *      * * * root    /usr/bin/php /opt/kito/ovpn/ovpn2json.php" >> /etc/crontab) && (/etc/init.d/cron reload)) || exit 30
-cat /etc/crontab | grep "/opt/kito/ovpn/ovpn2json2route.php" || (echo "* *      * * * root    /usr/bin/php /opt/kito/ovpn/ovpn2json2route.php" >> /etc/crontab) && (/etc/init.d/cron reload)) || exit 31
+cat /etc/crontab | grep "/opt/kito/ovpn/ovpn2json.php" || (echo "* *      * * * root    /usr/bin/php /opt/kito/ovpn/ovpn2json.php" >> /etc/crontab) && (/etc/init.d/cron reload) || exit 30
+cat /etc/crontab | grep "/opt/kito/ovpn/ovpn2json2route.php" || (echo "* *      * * * root    /usr/bin/php /opt/kito/ovpn/ovpn2json2route.php" >> /etc/crontab) && (/etc/init.d/cron reload) || exit 31
