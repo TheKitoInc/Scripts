bdd38fb1441ed7b102781f11040687d4033c1ac5
TheKitoInc <11139894+TheKito@users.noreply.github.com>
2023-10-25 18:07:47 -0300

    Update uptimeRobot.sh
    remove \r from dos files

--- uptimeRobot.sh
+++ uptimeRobot.sh
@@ -5,7 +5,7 @@ IPTABLES=$(which "iptables")
 IP6TABLES=$(which "ip6tables")
 
 $IPSET -exist -N uptimeRobotV4 hash:net
-$WGET --inet4-only -q https://uptimerobot.com/inc/files/ips/IPv4.txt -O - | while read ip
+$WGET --inet4-only -q https://uptimerobot.com/inc/files/ips/IPv4.txt -O - | tr -d '\r' | while read ip
 do
         $IPSET -exist -A uptimeRobotV4 $ip
 done
@@ -13,7 +13,7 @@ $IPTABLES -A INPUT  -p tcp --dport 80  -m set --match-set uptimeRobotV4 src -j A
 $IPTABLES -A INPUT  -p tcp --dport 443 -m set --match-set uptimeRobotV4 src -j ACCEPT
 
 $IPSET -exist -N uptimeRobotV6 hash:net family inet6
-$WGET --inet6-only -q https://uptimerobot.com/inc/files/ips/IPv6.txt -O - | while read ip
+$WGET --inet6-only -q https://uptimerobot.com/inc/files/ips/IPv6.txt -O - | tr -d '\r' | while read ip
 do
         $IPSET -exist -A uptimeRobotV6 $ip
 done
