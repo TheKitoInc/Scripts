a0ad776bd523fb6cc8da2a30f2877bf851853d35
Kito Git Service <git@thekitoinc.com>
2024-05-20 11:19:44 +0000

    exclude logs

--- Install/OpenVPNNode/ovpn2json.php
+++ Install/OpenVPNNode/ovpn2json.php
@@ -11,112 +11,102 @@ $json = '/var/log/openvpn.json';
 $section = null;
 $peers = array();
 
-foreach ($files as $file)
-{
-    foreach (explode("\n", str_replace("\r", "\n", file_get_contents($file))) as $line)
-    {
-        if ($line == '')
+foreach ($files as $file) {
+    foreach (explode("\n", str_replace("\r", "\n", file_get_contents($file))) as $line) {
+        if ($line == '') {
             continue;
+        }
 
-        if (strpos($line, 'END') !== FALSE)
-        {
+        if (strpos($line, 'END') !== false) {
             $section = null;
             continue;
-        }
-        elseif (strpos($line, 'CLIENT LIST') !== FALSE)
-        {
+        } elseif (strpos($line, 'CLIENT LIST') !== false) {
             $section = 'CLIENTS';
             continue;
-        }
-        elseif (strpos($line, 'ROUTING TABLE') !== FALSE)
-        {
+        } elseif (strpos($line, 'ROUTING TABLE') !== false) {
             $section = 'ROUTES';
             continue;
-        }
-        elseif (strpos($line, 'GLOBAL STATS') !== FALSE)
-        {
+        } elseif (strpos($line, 'GLOBAL STATS') !== false) {
             $section = 'STATS';
             continue;
         }
 
-        if ($section === null)
+        if ($section === null) {
             continue;
+        }
 
         $line = explode(',', $line);
 
-        if ($section == 'CLIENTS')
-        {
-            if (count($line) != 5)
+        if ($section == 'CLIENTS') {
+            if (count($line) != 5) {
                 continue;
+            }
 
             $virtual_address = null;
             $common_name = $line[0];
             $real_address = $line[1];
-        }
-        elseif ($section == 'ROUTES')
-        {
-            if (count($line) != 4)
+        } elseif ($section == 'ROUTES') {
+            if (count($line) != 4) {
                 continue;
+            }
 
             $virtual_address = $line[0];
             $common_name = $line[1];
             $real_address = $line[2];
-        }
-        elseif ($section == 'STATS')
-        {
+        } elseif ($section == 'STATS') {
             continue;
         }
 
 
         $_ = explode(':', $real_address);
-        if (!filter_var($_[0], FILTER_VALIDATE_IP))
+        if (!filter_var($_[0], FILTER_VALIDATE_IP)) {
             continue;
+        }
 
-        if (!isset($peers[$real_address]))
+        if (!isset($peers[$real_address])) {
             $peers[$real_address] = array();
+        }
 
         $peer = $peers[$real_address];
 
         $peer['common_name'] = $common_name;
 
-        if (!isset($peer['clients']))
+        if (!isset($peer['clients'])) {
             $peer['clients'] = array();
+        }
 
-        if (!isset($peer['networks']))
+        if (!isset($peer['networks'])) {
             $peer['networks'] = array();
+        }
 
-        if (!isset($peer['routes']))
+        if (!isset($peer['routes'])) {
             $peer['routes'] = array();
+        }
 
-        if (strpos($virtual_address, '/') !== FALSE)
-        {
+        if (strpos($virtual_address, '/') !== false) {
             $networks = $peer['networks'];
 
             $_ = explode('/', $virtual_address, 2);
 
-            if (count($_) == 2 && filter_var($_[0], FILTER_VALIDATE_IP) && is_numeric($_[1]) && $_[1] <= 64 && $_[1] >= 0 && !in_array($virtual_address, $networks))
+            if (count($_) == 2 && filter_var($_[0], FILTER_VALIDATE_IP) && is_numeric($_[1]) && $_[1] <= 64 && $_[1] >= 0 && !in_array($virtual_address, $networks)) {
                 $networks[] = $virtual_address;
+            }
 
             $peer['networks'] = $networks;
-        }
-        elseif (strpos($virtual_address, 'C') !== FALSE)
-        {
+        } elseif (strpos($virtual_address, 'C') !== false) {
             $clients = $peer['clients'];
 
             $_ = explode('C', $virtual_address, 2);
 
-            if (filter_var($_[0], FILTER_VALIDATE_IP) && !in_array($_[0], $clients))
+            if (filter_var($_[0], FILTER_VALIDATE_IP) && !in_array($_[0], $clients)) {
                 $clients[] = $_[0];
+            }
 
             $peer['clients'] = $clients;
-        }
-        elseif (filter_var($virtual_address, FILTER_VALIDATE_IP))
-        {
-                //var_dump($virtual_address);
+        } elseif (filter_var($virtual_address, FILTER_VALIDATE_IP)) {
+            //var_dump($virtual_address);
             $peer['next_hop'] = $virtual_address;
-        }
-        elseif ($virtual_address !== null)
-        {
+        } elseif ($virtual_address !== null) {
             print_r($line);
         }
 
@@ -127,5 +117,6 @@ foreach ($files as $file)
 
 $json_data = json_encode($peers);
 
-if (!file_exists($json) || file_get_contents($json) != $json_data)
+if (!file_exists($json) || file_get_contents($json) != $json_data) {
     file_put_contents($json, $json_data);
+}
