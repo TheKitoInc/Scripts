9c0bc11ecaa4701b7c6160724433d885165c4b2e
TheKitoInc <11139894+TheKitoInc@users.noreply.github.com>
2024-10-15 11:22:31 -0300

    Update install.bat

--- Zabbix/Agent/install.bat
+++ Zabbix/Agent/install.bat
@@ -9,7 +9,6 @@ if %errorLevel% == 0 (
 	powershell -Command "(New-Object Net.WebClient).DownloadFile('https://cdn.zabbix.com/zabbix/binaries/stable/7.0/7.0.4/zabbix_agent-7.0.4-windows-amd64-openssl.msi', '%MSIInstall%')"
 	msiexec /passive /i "%MSIInstall%" SERVER=127.0.0.1
 
-	
 	net stop "Zabbix Agent"
 
 	echo. > "%ProgramFiles%\Zabbix Agent\zabbix_agentd.conf"
