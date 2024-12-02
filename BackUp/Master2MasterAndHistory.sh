ab33b5e69d7c5ab0ef68107ced3c64869c940306
TheKitoInc <11139894+TheKitoInc@users.noreply.github.com>
2024-12-02 10:04:36 -0300

    Update Master2MasterAndHistory.sh

--- BackUp/Master2MasterAndHistory.sh
+++ BackUp/Master2MasterAndHistory.sh
@@ -2,11 +2,8 @@
 SRC_HOST=$1
 ssh-copy-id $SRC_HOST
 
-ssh $SRC_HOST "rmdir /Storage/Master/* 2> /dev/null"
-
 for SRC_NAME in $(ssh $SRC_HOST "ls /Storage/Master/")
 do
-
         SRC=$SRC_HOST:/Storage/Master/$SRC_NAME/
         DST=/Storage/Master/$SRC_NAME/
         HST=/Storage/History/$SRC_NAME/$(date "+%Y/%m/%d/%H")/
@@ -17,11 +14,7 @@ do
 
         mkdir -p "$DST" && \
         mkdir -p "$HST" && \
-        #rsync --dry-run -arv --delete-after --delete -b --backup-dir="$HST" "$SRC" "$DST" && \
-        rsync -arv --delete-after --delete -b --backup-dir="$HST" "$SRC" "$DST" && \
-        find "$HST" -type f -iname "*.bz2" -exec bunzip2 -fv "{}" \; && \
-        fdupes -drNI "$HST" && \
-        find "$HST" -type f ! -name "*.gz" ! -name "*.bz2" -exec gzip -fv "{}" \; && \
-        find "$HST" -type f -exec touch "{}" \;
+        rsync -arv --delete-after --delete -b --backup-dir="$HST" "$SRC" "$DST"
 
+        find "$HST" -type f -exec touch "{}" \;
 done
