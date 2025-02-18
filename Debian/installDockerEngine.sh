bde252c4ac06bdc8853211489b75ee92aed96509
TheKitoInc <11139894+TheKitoInc@users.noreply.github.com>
2025-02-18 09:38:11 -0300

    Update installDockerEngine.sh

--- Debian/installDockerEngine.sh
+++ Debian/installDockerEngine.sh
@@ -15,7 +15,7 @@ echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
   tee /etc/apt/sources.list.d/docker.list > /dev/null
-  
+
 # Update package list again
 apt-get update
 
