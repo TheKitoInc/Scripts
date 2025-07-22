7d362fa9dd2668e6af354bcaa0e4b5ef2687548c
TheKitoInc <11139894+TheKitoInc@users.noreply.github.com>
2025-07-22 08:45:34 -0300

    Update CloudFlare-IPs-IPSet-Load.sh

--- CloudFlare/CloudFlare-IPs-IPSet-Load.sh
+++ CloudFlare/CloudFlare-IPs-IPSet-Load.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+set -e
+
 WGET=$(which "wget")
 IPSET=$(which "ipset")
 IPTABLES=$(which "iptables")
@@ -9,13 +11,13 @@ $WGET --inet4-only -q https://www.cloudflare.com/ips-v4 -O - | while read ip
 do
         $IPSET -exist -A cloudFlareV4 $ip
 done
-$IPTABLES -A INPUT  -p tcp --dport 80  -m set --match-set cloudFlareV4 src -j ACCEPT
-$IPTABLES -A INPUT  -p tcp --dport 443 -m set --match-set cloudFlareV4 src -j ACCEPT
+$IPTABLES -S | grep -w "80"  | grep -w "cloudFlareV4" > /dev/null || $IPTABLES -A INPUT  -p tcp --dport 80  -m set --match-set cloudFlareV4 src -j ACCEPT
+$IPTABLES -S | grep -w "443" | grep -w "cloudFlareV4" > /dev/null || $IPTABLES -A INPUT  -p tcp --dport 443 -m set --match-set cloudFlareV4 src -j ACCEPT
 
 $IPSET -exist -N cloudFlareV6 hash:net family inet6
 $WGET --inet6-only -q https://www.cloudflare.com/ips-v6 -O - | while read ip
 do
         $IPSET -exist -A cloudFlareV6 $ip
 done
-$IP6TABLES -A INPUT -p tcp --dport 80  -m set --match-set cloudFlareV6 src -j ACCEPT
-$IP6TABLES -A INPUT -p tcp --dport 443 -m set --match-set cloudFlareV6 src -j ACCEPT
+$IP6TABLES -S | grep -w "80"  | grep -w "cloudFlareV6" > /dev/null || $IP6TABLES -A INPUT -p tcp --dport 80  -m set --match-set cloudFlareV6 src -j ACCEPT
+$IP6TABLES -S | grep -w "443" | grep -w "cloudFlareV6" > /dev/null || $IP6TABLES -A INPUT -p tcp --dport 443 -m set --match-set cloudFlareV6 src -j ACCEPT
