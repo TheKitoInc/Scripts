7b6cc674112dd800630289f1edcb4867a1747c6f
TheKitoInc <11139894+TheKitoInc@users.noreply.github.com>
2026-04-19 10:07:38 -0300

    Add Debloat.ps1 script to remove unwanted apps
    This script removes specified AppX applications from the system for debloating purposes.

---
+++ Windows/Debloat.ps1
@@ -0,0 +1,48 @@
+$AppXApps = @(
+        "*Microsoft.BingNews*"
+        "*Microsoft.GetHelp*"
+        "*Microsoft.Getstarted*"
+        "*Microsoft.Messaging*"
+        "*Microsoft.Microsoft3DViewer*"
+        "*Microsoft.MicrosoftOfficeHub*"
+        "*Microsoft.MicrosoftSolitaireCollection*"
+        "*Microsoft.NetworkSpeedTest*"
+        "*Microsoft.Office.Sway*"
+        "*Microsoft.OneConnect*"
+        "*Microsoft.People*"
+        "*Microsoft.Print3D*"
+        "*Microsoft.SkypeApp*"
+        "*Microsoft.WindowsAlarms*"
+        "*Microsoft.WindowsCamera*"
+        "*microsoft.windowscommunicationsapps*"
+        "*Microsoft.WindowsFeedbackHub*"
+        "*Microsoft.WindowsMaps*"
+        "*Microsoft.WindowsSoundRecorder*"
+        "*Microsoft.Xbox.TCUI*"
+        "*Microsoft.XboxApp*"
+        "*Microsoft.XboxGameOverlay*"
+        "*Microsoft.XboxIdentityProvider*"
+        "*Microsoft.XboxSpeechToTextOverlay*"
+        "*Microsoft.ZuneMusic*"
+        "*Microsoft.ZuneVideo*"
+        "*EclipseManager*"
+        "*ActiproSoftwareLLC*"
+        "*AdobeSystemsIncorporated.AdobePhotoshopExpress*"
+        "*Duolingo-LearnLanguagesforFree*"
+        "*PandoraMediaInc*"
+        "*CandyCrush*"
+        "*Wunderlist*"
+        "*Flipboard*"
+        "*Twitter*"
+        "*Facebook*"
+        "*Spotify*"
+        #"*Microsoft.Advertising.Xaml_10.1712.5.0_x64__8wekyb3d8bbwe*"
+        #"*Microsoft.Advertising.Xaml_10.1712.5.0_x86__8wekyb3d8bbwe*"
+        "*Microsoft.BingWeather*"                          
+    )
+    foreach ($App in $AppXApps) {
+        Write-Verbose -Message ('Removing Package {0}' -f $App)
+        Get-AppxPackage -Name $App | Remove-AppxPackage -ErrorAction SilentlyContinue
+        Get-AppxPackage -Name $App -AllUsers | Remove-AppxPackage -AllUsers -ErrorAction SilentlyContinue
+        Get-AppxProvisionedPackage -Online | Where-Object DisplayName -like $App | Remove-AppxProvisionedPackage -Online -ErrorAction SilentlyContinue
+    }
