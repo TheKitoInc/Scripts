f8b7bece4d0300d6cdf186224d194e66844404d2
TheKitoInc <11139894+TheKitoInc@users.noreply.github.com>
2026-05-19 10:55:52 -0300

    Create cleanPrinters.bat

---
+++ Windows/cleanPrinters.bat
@@ -0,0 +1,10 @@
+@echo off
+cls
+
+call "%~dp0showTitle.bat" "Printers Cleanup"
+
+rundll32 printui.dll,PrintUIEntry /q /dl /n "Fax"
+rundll32 printui.dll,PrintUIEntry /q /dl /n "AnyDesk Printer"
+rundll32 printui.dll,PrintUIEntry /q /dl /n "OneNote for Windows 10"
+rundll32 printui.dll,PrintUIEntry /q /dl /n "OneNote (Desktop)"
+rundll32 printui.dll,PrintUIEntry /q /dl /n "Microsoft XPS Document Writer"
