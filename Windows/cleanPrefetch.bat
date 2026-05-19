c6e63a34e8ff1b18cbe46749e256690017291496
TheKitoInc <11139894+TheKitoInc@users.noreply.github.com>
2026-05-19 10:48:45 -0300

    Update cleanPrefetch.bat

--- Windows/cleanPrefetch.bat
+++ Windows/cleanPrefetch.bat
@@ -4,6 +4,9 @@ cls
 :: =========================================
 :: PREFETCH
 :: =========================================
+
+call "%~dp0showTitle.bat" "Prefetch Cleaner"
+
 echo Cleaning Prefetch...
 
 del /f /s /q "C:\Windows\Prefetch\*" >nul 2>&1
