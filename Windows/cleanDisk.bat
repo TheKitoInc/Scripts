bd9cdde4e38109a0b2b67b92d77ebbb854c7293d
TheKitoInc <11139894+TheKitoInc@users.noreply.github.com>
2026-05-18 22:42:34 -0300

    Add cleanDisk.bat for disk cleanup automation

---
+++ Windows/cleanDisk.bat
@@ -0,0 +1,11 @@
+@echo off
+cls
+
+:: =========================================
+:: DISK CLEANUP
+:: =========================================
+echo Running Disk Cleanup...
+
+cleanmgr /sagerun:1
+
+echo.
