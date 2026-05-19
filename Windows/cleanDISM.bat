cf2b5551c73c0d3a5a8c743932358f5cf9f855a9
TheKitoInc <11139894+TheKitoInc@users.noreply.github.com>
2026-05-19 10:48:30 -0300

    Update cleanDISM.bat

--- Windows/cleanDISM.bat
+++ Windows/cleanDISM.bat
@@ -4,6 +4,9 @@ cls
 :: =========================================
 :: DISM COMPONENT CLEANUP
 :: =========================================
+
+call "%~dp0showTitle.bat" "DISM Component Cleanup"
+
 echo Running DISM component cleanup...
 
 DISM /Online /Cleanup-Image /StartComponentCleanup
