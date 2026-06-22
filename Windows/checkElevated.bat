cf60011bf1dc34758cf4065b3343f5d347b52764
TheKitoInc <11139894+TheKitoInc@users.noreply.github.com>
2026-06-22 13:38:34 -0300

    Update checkElevated.bat

--- Windows/checkElevated.bat
+++ Windows/checkElevated.bat
@@ -8,6 +8,6 @@ net session >nul 2>&1
 if %errorlevel% neq 0 (
     color 0C
     echo This script requires elevated privileges. Please run as administrator.   
-    timeout /t 5 /nobreak >nul 
-    exit /b
+    timeout /t 5
+    exit 
 )
