bbdd57525eb77379d607cb0d150c38c2538b61f1
TheKitoInc <11139894+TheKitoInc@users.noreply.github.com>
2026-06-22 14:09:32 -0300

    Update cleanChrome.bat

--- Windows/cleanChrome.bat
+++ Windows/cleanChrome.bat
@@ -15,4 +15,4 @@ rd /s /q "%LOCALAPPDATA%\Google\Chrome\User Data\Default\Cache" >nul 2>&1
 rd /s /q "%LOCALAPPDATA%\Google\Chrome\User Data\Default\Code Cache" >nul 2>&1
 rd /s /q "%LOCALAPPDATA%\Google\Chrome\User Data\Default\GPUCache" >nul 2>&1
 
-call "%~dp0Helpers\showDone.bat"
+call "%~dp0Helpers\showSuccess.bat"
