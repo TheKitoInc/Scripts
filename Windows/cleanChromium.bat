46fb62d298d899eaf734aac40b13526e94e980b5
TheKitoInc <11139894+TheKitoInc@users.noreply.github.com>
2026-06-22 13:14:08 -0300

    Update cleanChromium.bat

--- Windows/cleanChromium.bat
+++ Windows/cleanChromium.bat
@@ -15,5 +15,4 @@ rd /s /q "%LOCALAPPDATA%\Chromium\User Data\Default\Cache" >nul 2>&1
 rd /s /q "%LOCALAPPDATA%\Chromium\User Data\Default\Code Cache" >nul 2>&1
 rd /s /q "%LOCALAPPDATA%\Chromium\User Data\Default\GPUCache" >nul 2>&1
 
-echo Done.
-echo.
+call "%~dp0showDone.bat"
