703ab11b8df324e59d805329600f52545e71f9aa
TheKitoInc <11139894+TheKitoInc@users.noreply.github.com>
2026-06-22 13:57:37 -0300

    Update cleanPrinters.bat

--- Windows/cleanPrinters.bat
+++ Windows/cleanPrinters.bat
@@ -1,7 +1,7 @@
 @echo off
 cls
 
-call "%~dp0showTitle.bat" "Printers Cleanup"
+call "%~dp0Helpers\showTitle.bat" "Printers Cleanup"
 
 rundll32 printui.dll,PrintUIEntry /q /dl /n "Fax"
 rundll32 printui.dll,PrintUIEntry /q /dl /n "AnyDesk Printer"
@@ -9,4 +9,4 @@ rundll32 printui.dll,PrintUIEntry /q /dl /n "OneNote for Windows 10"
 rundll32 printui.dll,PrintUIEntry /q /dl /n "OneNote (Desktop)"
 rundll32 printui.dll,PrintUIEntry /q /dl /n "Microsoft XPS Document Writer"
 
-call "%~dp0showDone.bat"
+call "%~dp0Helpers\showDone.bat"
