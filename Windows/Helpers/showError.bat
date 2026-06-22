d4195094e421f41a2d4db0a83d3ff79e6ae5d6a6
TheKitoInc <11139894+TheKitoInc@users.noreply.github.com>
2026-06-22 14:08:22 -0300

    Update showError.bat

--- Windows/Helpers/showError.bat
+++ Windows/Helpers/showError.bat
@@ -6,8 +6,9 @@
 
 :: Set color to red
 color 0C
-echo An error occurred during the execution of the script.
-echo '%~f1'
+echo An error occurred during the execution of the script:
+echo "%~f0"
+echo %~1
 timeout /t 5
 
 cls
