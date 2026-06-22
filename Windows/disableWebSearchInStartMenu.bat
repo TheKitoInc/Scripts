9c462052a605f3fd84b264dc8b365096676858cc
TheKitoInc <11139894+TheKitoInc@users.noreply.github.com>
2026-06-22 13:14:54 -0300

    Update disableWebSearchInStartMenu.bat

--- Windows/disableWebSearchInStartMenu.bat
+++ Windows/disableWebSearchInStartMenu.bat
@@ -1,6 +1,12 @@
 @echo off
 cls
 
+:: =========================================
+:: DISABLE WEB SEARCH IN START MENU
+:: =========================================
+
 call "%~dp0showTitle.bat" "Disable Web Search in Start Menu"
 
 powershell -ExecutionPolicy Bypass "%~dp0%~n0.ps1"
+
+call "%~dp0showDone.bat"
