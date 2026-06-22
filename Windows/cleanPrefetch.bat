26aa00f9e629d9068485779b09174633412adf3f
TheKitoInc <11139894+TheKitoInc@users.noreply.github.com>
2026-06-22 13:57:35 -0300

    Update cleanPrefetch.bat

--- Windows/cleanPrefetch.bat
+++ Windows/cleanPrefetch.bat
@@ -5,12 +5,12 @@ cls
 :: PREFETCH
 :: =========================================
 
-call "%~dp0showTitle.bat" "Prefetch Cleaner"
+call "%~dp0Helpers\showTitle.bat" "Prefetch Cleaner"
 
-call "%~dp0checkElevated.bat"
+call "%~dp0Helpers\checkElevated.bat"
 
 echo Cleaning Prefetch...
 
 del /f /s /q "C:\Windows\Prefetch\*" >nul 2>&1
 
-call "%~dp0showDone.bat"
+call "%~dp0Helpers\showDone.bat"
