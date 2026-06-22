036935923eb5c20d902466d676f2ce4be0813fb7
TheKitoInc <11139894+TheKitoInc@users.noreply.github.com>
2026-06-22 14:22:10 -0300

    Update cleanEdge.bat

--- Windows/cleanEdge.bat
+++ Windows/cleanEdge.bat
@@ -14,5 +14,6 @@ taskkill /f /im msedge.exe >nul 2>&1
 rd /s /q "%LOCALAPPDATA%\Microsoft\Edge\User Data\Default\Cache" >nul 2>&1
 rd /s /q "%LOCALAPPDATA%\Microsoft\Edge\User Data\Default\Code Cache" >nul 2>&1
 rd /s /q "%LOCALAPPDATA%\Microsoft\Edge\User Data\Default\GPUCache" >nul 2>&1
+rd /s /q "%LOCALAPPDATA%\Microsoft\Edge\User Data\Default\Service Worker\CacheStorage" >nul 2>&1
 
 call "%~dp0Helpers\showSuccess.bat"
