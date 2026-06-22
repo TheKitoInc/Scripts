d9ab95390511bc7aa539b9917d538f36e081e57f
TheKitoInc <11139894+TheKitoInc@users.noreply.github.com>
2026-06-22 13:48:37 -0300

    Create move.bat

---
+++ Windows/move.bat
@@ -0,0 +1,16 @@
+@echo off
+cls
+
+:: =========================================
+:: MOVE FILES AND FOLDERS
+:: =========================================
+
+call "%~dp0showTitle.bat" "Moving %~1 > %~2"
+
+SET MoveDirSource=%~1
+SET MoveDirDestination=%~2
+MKDIR "%MoveDirDestination%"
+FOR    %%i IN ("%MoveDirSource%\*") DO           MOVE /Y "%%i" "%MoveDirDestination%\%%~nxi"
+FOR /D %%i IN ("%MoveDirSource%\*") DO ROBOCOPY /MOVE /E "%%i" "%MoveDirDestination%\%%~nxi"
+
+call "%~dp0showDone.bat"
