2a31ed95e84a74cefce5da4a4565782c5a5980b2
TheKitoInc <11139894+TheKitoInc@users.noreply.github.com>
2026-06-22 13:59:00 -0300

    Update disableWebSearchInStartMenu.bat

--- Windows/disableWebSearchInStartMenu.bat
+++ Windows/disableWebSearchInStartMenu.bat
@@ -5,9 +5,9 @@ cls
 :: DISABLE WEB SEARCH IN START MENU
 :: =========================================
 
-call "%~dp0showTitle.bat" "Disable Web Search in Start Menu"
+call "%~dp0Helpers\showTitle.bat" "Disable Web Search in Start Menu"
 
 reg add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Search" /v BingSearchEnabled /t REG_DWORD /d 0 /f >nul 2>&1
 reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\Windows Search" /v DisableWebSearch /t REG_DWORD /d 1 /f >nul 2>&1
 
-call "%~dp0showDone.bat"
+call "%~dp0Helpers\showDone.bat"
