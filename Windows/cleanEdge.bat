687c58358dceb1a49b451102ea3e3feab59f982d
TheKitoInc <11139894+TheKitoInc@users.noreply.github.com>
2026-06-22 13:14:16 -0300

    Update cleanEdge.bat

--- Windows/cleanEdge.bat
+++ Windows/cleanEdge.bat
@@ -15,5 +15,4 @@ rd /s /q "%LOCALAPPDATA%\Microsoft\Edge\User Data\Default\Cache" >nul 2>&1
 rd /s /q "%LOCALAPPDATA%\Microsoft\Edge\User Data\Default\Code Cache" >nul 2>&1
 rd /s /q "%LOCALAPPDATA%\Microsoft\Edge\User Data\Default\GPUCache" >nul 2>&1
 
-echo Done.
-echo.
+call "%~dp0showDone.bat"
