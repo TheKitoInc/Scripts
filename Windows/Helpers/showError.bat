9a42f0d47a04d61d3aca9908f240bbaf88635b8e
TheKitoInc <11139894+TheKitoInc@users.noreply.github.com>
2026-06-22 14:05:02 -0300

    Create showError.bat

---
+++ Windows/Helpers/showError.bat
@@ -0,0 +1,13 @@
+@echo off
+
+:: =========================================
+:: ERROR MESSAGE
+:: =========================================
+
+:: Set color to red
+color 0C
+echo An error occurred during the execution of the script.
+echo '%~f1'
+timeout /t 5
+
+cls
