1cb1f5f21421b44f5ae21b2b3548b925b5e9f515
TheKitoInc <11139894+TheKitoInc@users.noreply.github.com>
2026-06-22 13:57:39 -0300

    Update cleanRecycleBin.bat

--- Windows/cleanRecycleBin.bat
+++ Windows/cleanRecycleBin.bat
@@ -5,10 +5,10 @@ cls
 :: RECYCLE BIN
 :: =========================================
 
-call "%~dp0showTitle.bat" "Recycle Bin Cleaner"
+call "%~dp0Helpers\showTitle.bat" "Recycle Bin Cleaner"
 
 echo Emptying Recycle Bin...
 
 powershell -Command "Clear-RecycleBin -Force" >nul 2>&1
 
-call "%~dp0showDone.bat"
+call "%~dp0Helpers\showDone.bat"
