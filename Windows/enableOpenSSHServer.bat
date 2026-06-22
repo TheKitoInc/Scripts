0863bfe41669e1efc5dfae335b63ba0664c6c285
TheKitoInc <11139894+TheKitoInc@users.noreply.github.com>
2026-06-22 13:14:59 -0300

    Update enableOpenSSHServer.bat

--- Windows/enableOpenSSHServer.bat
+++ Windows/enableOpenSSHServer.bat
@@ -1,6 +1,12 @@
 @echo off
 cls
 
+:: =========================================
+:: ENABLE OPENSSH SERVER
+:: =========================================
+
 call "%~dp0showTitle.bat" "Enable OpenSSH Server"
 
 powershell -ExecutionPolicy Bypass "%~dp0%~n0.ps1"Server
+
+call "%~dp0showDone.bat"
