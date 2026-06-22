4d4eccdf968b3c3115894a08eade636aad326954
TheKitoInc <11139894+TheKitoInc@users.noreply.github.com>
2026-06-22 13:14:49 -0300

    Update disableNewsFeeds.bat

--- Windows/disableNewsFeeds.bat
+++ Windows/disableNewsFeeds.bat
@@ -1,8 +1,14 @@
 @echo off
 cls
 
+:: =========================================
+:: DISABLE NEWS AND INTERESTS
+:: =========================================
+
 call "%~dp0showTitle.bat" "Disable News and Interests"
 
 taskkill /F /IM explorer.exe
 reg add "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Feeds" /v "ShellFeedsTaskbarPreviousViewMode" /t REG_DWORD /d "2" /f
 start explorer.exe
+
+call "%~dp0showDone.bat"
